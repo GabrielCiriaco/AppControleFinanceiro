8b30bb2aa56f1fff08755d534a14e1ae40a276f9
unknown <gabrielcfornitano@gmail.com>
2022-06-28 21:48:23 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'payroll',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
